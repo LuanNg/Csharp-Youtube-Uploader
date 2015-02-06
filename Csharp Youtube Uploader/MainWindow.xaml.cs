b748f336d5f15fb287b9d6d78d21e0e163d0d134
Dragonixxx <dragonixx@web.de>
2015-02-06 14:03:18 +0100

    Language Button DESIGN
    not functional yet

--- Csharp Youtube Uploader/MainWindow.xaml.cs
+++ Csharp Youtube Uploader/MainWindow.xaml.cs
@@ -42,6 +42,14 @@ namespace Csharp_Youtube_Uploader
 
 		}
 
+		private void LanguageButton_Click(object sender, RoutedEventArgs e)
+		{
+			(sender as Button).ContextMenu.IsEnabled = true;
+			(sender as Button).ContextMenu.PlacementTarget = (sender as Button);
+			(sender as Button).ContextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+			(sender as Button).ContextMenu.IsOpen = true;
+		}
+
 		
 
 	}
